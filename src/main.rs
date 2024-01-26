acc4d763ffd110fcb56901760b45ae7bb507df2f
Pedro Manse <pedro.manse@dmk3.com.br>
2024-01-26 16:16:25 -0300

    Library system

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+// library system
+
+mod time;
+mod sql;
+//mod client {}
+//mod worker {}
+
+fn ex<A, E: ToString>(a: Result<A, E>) -> Result<A, String> {
+	a.map_err(|e|e.to_string())
+}
+
+use std::str::FromStr;
+fn main() -> Result<(), String> {
+	let conn = ex(sql::open("db"))?;
+	ex(conn.schema(sql::SQL_TABLE_SCHEMA))?;
+
+	//let pub_date = ex(time::Date::from_str("1-12-1887"))?;
+	//let b = ex(Book::new(&conn, 9780140439083, "A Study In Scarlet".to_string(), vec![], pub_date))?;
+
+	let mut stmt = ex(conn.prepare("SELECT * FROM books"))?;
+	let rows = ex(stmt.query_map([], Book::from_query))?;
+	let bks: Vec<Book> = rows.filter(|a|a.is_ok()).map(|b|b.unwrap()).collect();
+	println!("{:#?}", bks);
+
+	Ok(())
+}
+
+#[derive(Debug)]
+#[allow(non_snake_case, dead_code)] // for ISBN
+pub struct Book {
+	ISBN: u64,
+	internal_id: i64,
+
+	name: String,
+	authors: Vec<String>,
+	published: time::Date,
+
+	last_borrow: Option<time::Date>,
+	borrower_id: Option<u64>, // ID of current borrower
+}
+
+impl Book {
+	pub fn from_query(query: &rusqlite::Row) -> Result<Book, rusqlite::Error> {
+		let ISBN:u64 = query.get(0)?;
+		let internal_id:i64 = query.get(1)?;
+		let name:String = query.get(2)?;
+		let authors = vec![]; // TODO read authors
+		let pub_date:String = query.get(3)?;
+		let published = time::Date::from_str(&pub_date).map_err(|_|rusqlite::Error::InvalidQuery)?;
+		let last_borrow_date:Option<String> = query.get(4)?;
+		let last_borrow = last_borrow_date.map(|b_date|time::Date::from_str(&b_date).unwrap());
+		let borrower_id:Option<u64> = query.get(5)?;
+
+		Ok(Book{
+			ISBN, internal_id, name,
+			authors, published,
+			last_borrow, borrower_id,
+		})
+	}
+
+	pub fn new(
+		db: &sql::DB,
+		ISBN: u64, name: String,
+		authors: Vec<String>, published: time::Date
+	) -> Result<Book, rusqlite::Error> {
+		// TODO authors in sql too!
+		let internal_id = Book::add_to_db(db, ISBN, &name, published)?;
+
+		Ok(Book{
+			ISBN, internal_id, name,
+			authors, published,
+			last_borrow: None, borrower_id: None,
+		})
+	}
+
+	fn add_to_db(
+		db: &sql::DB,
+		ISBN: u64, name: &str, published: time::Date
+	) -> Result<i64, rusqlite::Error> {
+		db.insert(r#"
+INSERT INTO books
+	(ISBN, name, published)
+VALUES
+	(?, ?, ?)
+		"#, (
+			ISBN, name,
+			published,
+		))
+	}
+}
+
---
+++ src/sql.rs
@@ -0,0 +1,62 @@
+pub struct DB(rusqlite::Connection);
+
+pub fn open(path: &str) -> Result<DB, rusqlite::Error> {
+	if path==":memory:" {
+		rusqlite::Connection::open_in_memory().map(|con|DB(con))
+	} else {
+		rusqlite::Connection::open(path).map(|con|DB(con))
+	}
+}
+
+#[allow(dead_code)]
+impl DB {
+	pub fn schema(&self, command: &str) -> Result<usize, rusqlite::Error> {
+		self.0.execute(command, ())
+	}
+
+	// on Ok returns number of rows edited
+	pub fn exec<P: rusqlite::Params>(&self, command: &str, params: P) -> Result<usize, rusqlite::Error> {
+		self.0.prepare_cached(command)?.execute(params)
+	}
+
+	// on Ok returns last insert id
+	pub fn insert<P: rusqlite::Params>(&self, command: &str, params: P) -> Result<i64, rusqlite::Error> {
+		self.0.prepare_cached(command)?.insert(params)
+	}
+
+	pub fn last_insert_id(&self) -> i64 {
+		self.0.last_insert_rowid()
+	}
+
+	pub fn close(self) -> Result<(), (DB, rusqlite::Error)> {
+		self.0.close().map_err(|(con, e)| (DB(con), e) )
+	}
+
+	pub fn prepare(&self, command: &str) -> Result<rusqlite::CachedStatement, rusqlite::Error> {
+		self.0.prepare_cached(command)
+	}
+}
+
+pub const SQL_TABLE_SCHEMA: &str = r#"
+CREATE TABLE IF NOT EXISTS books (
+	ISBN INTEGER NOT NULL,
+	book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	published DATE NOT NULL,
+	last_borrow DATE,
+	borrower_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS authors (
+	id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS wrote (
+	author_id INTEGER NOT NULL,
+	book_id INTEGER NOT NULL,
+	UNIQUE(author_id, book_id),
+	FOREIGN KEY(author_id) REFERENCES authors(id),
+	FOREIGN KEY(book_id) REFERENCES books(ISBN)
+);
+"#;
